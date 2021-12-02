9b9a8b4ebbbd00dc892125f47e291defc02e0ae5
Dave <d.r.young@qub.ac.uk>
2021-12-02 12:52:09 +0000

    adding tests for daily_max() and daily_min()

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,8 +1,8 @@
 """Tests for statistics functions within the Model layer."""
 
+import pytest
 import numpy as np
 import numpy.testing as npt
-
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -29,5 +29,34 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+def test_daily_max():
+    """Test that max function works for an array of positive integers."""
+    from inflammation.models import daily_max
+
+    test_input = np.array([[4, 2, 5],
+                           [1, 6, 2],
+                           [4, 1, 9]])
+    test_result = np.array([4, 6, 9])
+
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_min():
+    """Test that min function works for an array of positive and negative integers."""
+    from inflammation.models import daily_min
+
+    test_input = np.array([[ 4, -2, 5],
+                           [ 1, -6, 2],
+                           [-4, -1, 9]])
+    test_result = np.array([-4, -6, 2])
+
+    npt.assert_array_equal(daily_min(test_input), test_result)
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
 
 # TODO(lesson-robust) Implement tests for the other statistical functions
